455b0c8a33ed20ff2a599ec206cb6510c2a8ab05
John Dunham <jfdbear1@gmail.com>
2020-11-10 10:45:09 -0500

    Improving pretty print.

--- project.m
+++ project.m
@@ -1,15 +1,17 @@
-function [percentages] = project(num_samples)
+function [] = project(num_samples)
 N_list = [ 10, 100, 1000, 10000 ];
 %alpha_list = [.25 .1 .05 .01];
 num_Ns = size(N_list,2);
-percentages = {};
+%percentages = {};
 
 
 for i = 1:num_Ns
-    disp("==============================================")
-    disp(N_list(i))
-    disp("----------------------------------------------")
+    disp("==============================================");
+    disp("N = " + N_list(i) + " Sample = " + num_samples);
+    disp("----------------------------------------------");
     project_test_N(N_list(i),num_samples)
+    disp("++++++++++++++++++++++++++++++++++++++++++++++");
+
 end
 
 
