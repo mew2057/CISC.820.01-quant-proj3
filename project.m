8b5a7070bfd5c2c3cc3bb2665ec86d51bcdd9621
John Dunham <jfdbear1@gmail.com>
2020-11-11 09:42:53 -0500

    The project _clearly_ said [0,1] I am dumb.

--- project.m
+++ project.m
@@ -1,16 +1,17 @@
 function [] = project(num_samples)
-N_list = [ 10, 100, 1000, 10000 ];
+N_list = [ 10, 100, 1000, 10000, 100000 ];
 %alpha_list = [.25 .1 .05 .01];
 num_Ns = size(N_list,2);
 %percentages = {};
 
 uni_min = 0;
 uni_max = 1;
+% 
+% norm_sig = 1;
+% norm_mu  = .5;
 
-norm_sig = 1;
-norm_mu  = .5;
+bern_theta = .95;
 
-bern_theta = .5;
 
 disp( " Uniform Sample (" + uni_min + "," + uni_max + ")");
 for i = 1:num_Ns
@@ -21,17 +22,18 @@ for i = 1:num_Ns
     disp("++++++++++++++++++++++++++++++++++++++++++++++");
 end
 
-disp("==============================================");
-disp("Normal Sample (sig=" + norm_sig + ", mu=" + norm_mu + ")");
-for i = 1:num_Ns
-    N=N_list(i);    
-    disp("N = " + N + " Sample = " + num_samples);
-    disp("----------------------------------------------");
-    project_test(num_samples,sample_normal(N,norm_sig, norm_mu))
-    disp("++++++++++++++++++++++++++++++++++++++++++++++");
-end
-disp("==============================================");
+% disp("==============================================");
+% disp("Normal Sample (sig=" + norm_sig + ", mu=" + norm_mu + ")");
+% for i = 1:num_Ns
+%     N=N_list(i);    
+%     disp("N = " + N + " Sample = " + num_samples);
+%     disp("----------------------------------------------");
+%     project_test(num_samples,sample_normal(N,norm_sig, norm_mu))
+%     disp("++++++++++++++++++++++++++++++++++++++++++++++");
+% end
 
+disp("==============================================");
+% Characteristic [0,1]
 disp("Bernoulli Sample (theta=" + bern_theta +")");
 for i = 1:num_Ns
     N=N_list(i);    
